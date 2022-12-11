c0478d60a8ec18edf68bfc0328672ec75f746d45
Helin <helintoptanci3@outlook.com>
2022-12-11 18:06:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(results) {
+    
+    const win = results.find(result => result.result === "W");
+  
+   
+    if (win) {
+      return win.year;
+    }
+  
+    
+    return undefined;
+  }
+  
